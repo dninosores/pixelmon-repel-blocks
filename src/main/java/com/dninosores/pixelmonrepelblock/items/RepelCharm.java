5366b176e47797efac5df5b6fb4dfba8f36f8758
dninosores <dninosores@gmail.com>
2023-10-21 23:17:37 -0700

    Repel charm now works as a normal item

--- src/main/java/com/dninosores/pixelmonrepelblock/items/RepelCharm.java
+++ src/main/java/com/dninosores/pixelmonrepelblock/items/RepelCharm.java
@@ -4,30 +4,69 @@ import com.dninosores.pixelmonrepelblock.ModFile;
 import com.dninosores.pixelmonrepelblock.blocks.RepelItemGroup;
 import com.pixelmonmod.pixelmon.api.spawning.AbstractSpawner;
 import com.pixelmonmod.pixelmon.api.spawning.SpawnInfo;
+import com.pixelmonmod.pixelmon.entities.effects.RepelEffect;
+import com.pixelmonmod.pixelmon.init.registry.EffectRegistration;
 import com.pixelmonmod.pixelmon.items.LureItem;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.ItemUseContext;
+import net.minecraft.nbt.CompoundNBT;
+import net.minecraft.nbt.INBT;
+import net.minecraft.nbt.IntNBT;
+import net.minecraft.nbt.NumberNBT;
+import net.minecraft.potion.EffectInstance;
+import net.minecraft.util.ActionResult;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.world.World;
 
 import java.lang.reflect.Field;
 
-public class RepelCharm extends LureItem {
-    public RepelCharm() {
-        super(LureType.HA, LureStrength.STRONG);
-        try {
-            Field categoryField = Item.class.getDeclaredField("category");
-            categoryField.setAccessible(true);
-            categoryField.set(this, RepelItemGroup.REPEL_GROUP);
+public class RepelCharm extends Item {
+    private final String ACTIVE = "active";
 
-            Field maxDamageField = Item.class.getDeclaredField("maxDamage");
-            maxDamageField.setAccessible(true);
-            maxDamageField.set(this, -1);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
+    public RepelCharm() {
+        super(new Item.Properties().tab(RepelItemGroup.REPEL_GROUP));
+    }
+
+    @Override
+    public ItemStack getDefaultInstance() {
+        ItemStack newstack = super.getDefaultInstance();
+        CompoundNBT nbtTagCompound = new CompoundNBT();
+        nbtTagCompound.putBoolean(ACTIVE, false);
+        newstack.setTag(nbtTagCompound);
+        return newstack;
+    }
+
+    @Override
+    public ActionResultType onItemUseFirst(ItemStack stack, ItemUseContext context) {
+        ModFile.LOGGER.info("CLICKED");
+        CompoundNBT tags = stack.getTag();
+        tags.putBoolean(ACTIVE, !tags.getBoolean(ACTIVE));
+
+        if (tags.getBoolean(ACTIVE)) {
+            tags.putString("ench", "something");
+        } else if (tags.contains("ench")){
+            tags.remove("ench");
         }
+        stack.setTag(tags);
+
+        ModFile.LOGGER.info(tags.getBoolean(ACTIVE));
+
+        return ActionResultType.SUCCESS;
 
     }
 
-    public float getMultiplier(AbstractSpawner spawner, SpawnInfo spawnInfo, float sum, float rarity) {
-        return 0;
+    @Override
+    public void inventoryTick(ItemStack stack, World p_77663_2_, Entity entity, int p_77663_4_, boolean p_77663_5_) {
+        if (stack.getTag().contains(ACTIVE) && stack.getTag().getBoolean(ACTIVE)) {
+            if (entity instanceof LivingEntity) {
+                LivingEntity livingEntity = (LivingEntity) entity;
+                livingEntity.addEffect(new EffectInstance(EffectRegistration.REPEL.get(), 5));
+            }
+        }
     }
 }
