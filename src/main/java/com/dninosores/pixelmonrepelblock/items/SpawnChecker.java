251853e7f6981fb1604f13e053b20da0bbbcadae
dninosores <dninosores@gmail.com>
2023-10-22 00:50:21 -0700

    Spawn checker works with pixelmon command

--- src/main/java/com/dninosores/pixelmonrepelblock/items/SpawnChecker.java
+++ src/main/java/com/dninosores/pixelmonrepelblock/items/SpawnChecker.java
@@ -1,18 +1,11 @@
 package com.dninosores.pixelmonrepelblock.items;
 
-import com.dninosores.pixelmonrepelblock.ModFile;
 import com.dninosores.pixelmonrepelblock.blocks.RepelItemGroup;
 import com.pixelmonmod.pixelmon.api.spawning.AbstractSpawner;
-import com.pixelmonmod.pixelmon.api.spawning.calculators.CheckSpawns;
-import com.pixelmonmod.pixelmon.command.impl.CheckSpawnsCommand;
 import com.pixelmonmod.pixelmon.spawning.PixelmonSpawning;
-import net.minecraft.command.CommandSource;
-import net.minecraft.command.Commands;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
-import net.minecraft.nbt.CompoundNBT;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
 import net.minecraft.world.World;
