f472e646aaaa123d9175e47ef0817ec7118806a5
dninosores <dninosores@gmail.com>
2023-10-22 01:05:36 -0700

    Spawn checker works with repel block

--- src/main/java/com/dninosores/pixelmonrepelblock/items/SpawnChecker.java
+++ src/main/java/com/dninosores/pixelmonrepelblock/items/SpawnChecker.java
@@ -1,5 +1,7 @@
 package com.dninosores.pixelmonrepelblock.items;
 
+import com.dninosores.pixelmonrepelblock.ModFile;
+import com.dninosores.pixelmonrepelblock.Utils;
 import com.dninosores.pixelmonrepelblock.blocks.RepelItemGroup;
 import com.pixelmonmod.pixelmon.api.spawning.AbstractSpawner;
 import com.pixelmonmod.pixelmon.spawning.PixelmonSpawning;
@@ -8,9 +10,12 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Hand;
+import net.minecraft.util.math.vector.Vector3d;
+import net.minecraft.util.text.StringTextComponent;
 import net.minecraft.world.World;
 
 import java.util.ArrayList;
+import java.util.Map;
 
 public class SpawnChecker extends Item {
     public SpawnChecker() {
@@ -21,8 +26,19 @@ public class SpawnChecker extends Item {
     public ActionResult<ItemStack> use(World w, PlayerEntity player, Hand hand) {
         ItemStack stack = player.getItemInHand(hand);
         if (w.isClientSide()) {
-            AbstractSpawner spawner = PixelmonSpawning.coordinator.getSpawner(player.getName().getString());
-            spawner.checkSpawns.checkSpawns(spawner, player.createCommandSourceStack(), new ArrayList<>());
+            boolean blocked = false;
+            for (Map.Entry<Vector3d, Integer> entry : ModFile.getSpawnBlockLocations().entrySet()) {
+                if (Utils.inCube(entry.getValue(), entry.getKey(), player.position())) {
+                    player.sendMessage(new StringTextComponent(String.format("Spawning blocked by repel block at: (%s, %s, %s)",
+                            entry.getKey().x, entry.getKey().y, entry.getKey().z)), player.getUUID());
+                    blocked = true;
+                }
+            }
+
+            if (!blocked) {
+                AbstractSpawner spawner = PixelmonSpawning.coordinator.getSpawner(player.getName().getString());
+                spawner.checkSpawns.checkSpawns(spawner, player.createCommandSourceStack(), new ArrayList<>());
+            }
         }
         return ActionResult.success(stack);
 
